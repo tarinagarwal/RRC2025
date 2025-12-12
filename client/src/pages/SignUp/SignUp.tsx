e35f89a43259acefc546dab79749089d574f62ba
tarinagarwal <tarinagarwal@users.noreply.github.com>
2025-12-12 15:18:14 +0000

    feat(client): add signup page

---
+++ client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,205 @@
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { AlertCircle, Eye, EyeOff, LinkIcon, Loader2 } from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { signupSchema } from "@/validation/userSchema";
+import type { signupUser } from "@/validation/userSchema";
+import { useForm } from "react-hook-form";
+import type { SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import SocialButtons from "@/components/Auth/SocialButtons";
+import { signUp } from "@/api/authService";
+import type { ErrorResponse } from "@/types/auth";
+import { AxiosError } from "axios";
+
+type signupFields = signupUser;
+
+const SignUpForm: React.FC = () => {
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<signupFields>({ resolver: zodResolver(signupSchema) });
+
+  const onSubmit: SubmitHandler<signupFields> = async (data) => {
+    try {
+      const response = await signUp(data);
+
+      if (response.data.success && !response.data.isVerified) {
+        navigate(`/verifymail?email=${data.email}`);
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ErrorResponse>;
+      if (axiosError.response && axiosError.response.data) {
+        const backendError = axiosError.response.data.message;
+        console.error("Error:", backendError);
+        setError("root", { message: backendError });
+      }
+    }
+  };
+
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center p-4 relative overflow-hidden bg-gradient-to-br from-[#F9F6EE] via-[#EFE7D4] to-[#E4D7B4]">
+      <div
+        className="absolute inset-0 z-0 opacity-30"
+        style={{
+          backgroundImage: `url("data:image/svg+xml,%3Csvg width='100' height='100' viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M11 18c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm48 25c3.866 0 7-3.134 7-7s-3.134-7-7-7-7 3.134-7 7 3.134 7 7 7zm-43-7c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm63 31c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM34 90c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zm56-76c1.657 0 3-1.343 3-3s-1.343-3-3-3-3 1.343-3 3 1.343 3 3 3zM12 86c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm28-65c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm23-11c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-6 60c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm29 22c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zM32 63c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm57-13c2.76 0 5-2.24 5-5s-2.24-5-5-5-5 2.24-5 5 2.24 5 5 5zm-9-21c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM60 91c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM35 41c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2zM12 60c1.105 0 2-.895 2-2s-.895-2-2-2-2 .895-2 2 .895 2 2 2z' fill='%23ffffff' fill-opacity='0.1' fill-rule='evenodd'/%3E%3C/svg%3E")`,
+          backgroundSize: "100px 100px",
+        }}
+      />
+
+      <div className="w-full max-w-xl  z-10 flex items-center justify-center">
+        <Card className="w-full h-full backdrop-blur-sm bg-white shadow-xl border-2 border-[#E4D7B4]">
+          <CardHeader className="space-y-1 flex flex-col items-center pt-8">
+            <div className="flex items-center space-x-3 mb-3">
+              <div className="w-12 h-12 bg-gradient-to-tr from-[#335441] to-[#46704A] rounded-xl flex items-center justify-center shadow-lg">
+                <LinkIcon className="text-white w-6 h-6" />
+              </div>
+              <CardTitle className="text-3xl font-bold text-[#335441]">
+                PrepX
+              </CardTitle>
+            </div>
+          </CardHeader>
+          <CardContent className="space-y-6 px-8 py-5">
+            <div className="space-y-2 text-center">
+              <h2 className="text-3xl font-semibold tracking-tight text-[#335441]">
+                Sign Up
+              </h2>
+              <p className="text-sm text-[#6B8F60]">
+                Enter your information to create an account
+              </p>
+              {errors.root && (
+                <div className="flex items-center bg-red-100 border-l-4 border-red-500 text-red-700 p-4 rounded-md">
+                  <AlertCircle className="w-5 h-5 mr-3" />
+
+                  <span>{errors.root.message}</span>
+                </div>
+              )}
+            </div>
+            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+              <div className="space-y-2">
+                <Label
+                  htmlFor="username"
+                  className="text-sm font-medium text-[#335441]"
+                >
+                  Username
+                </Label>
+                <Input
+                  {...register("username")}
+                  id="username"
+                  placeholder="John Doe"
+                  required
+                  className="transition-all duration-200 focus:ring-2 focus:ring-[#335441] border-2 border-[#E4D7B4]"
+                />
+                {errors.username && (
+                  <p className="text-red-500">{errors.username.message}</p>
+                )}
+              </div>
+              <div className="space-y-2">
+                <Label
+                  htmlFor="email"
+                  className="text-sm font-medium text-[#335441]"
+                >
+                  Email
+                </Label>
+                <Input
+                  {...register("email")}
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  className="transition-all duration-200 focus:ring-2 focus:ring-[#335441] border-2 border-[#E4D7B4]"
+                />
+                {errors.email && (
+                  <p className="text-red-500">{errors.email.message}</p>
+                )}
+              </div>
+              <div className="space-y-2">
+                <Label
+                  htmlFor="password"
+                  className="text-sm font-medium text-[#335441]"
+                >
+                  Password
+                </Label>
+
+                <div className="space-y-2">
+                  <div className="relative">
+                    <Input
+                      {...register("password")}
+                      id="password"
+                      type={showPassword ? "text" : "password"}
+                      placeholder="Your password"
+                      required
+                      className="transition-all duration-200 focus:ring-2 focus:ring-[#335441] border-2 border-[#E4D7B4] pr-10"
+                    />
+                    <button
+                      type="button"
+                      onClick={togglePasswordVisibility}
+                      className="absolute inset-y-0 right-0 pr-3 flex items-center text-[#6B8F60] hover:text-[#335441]"
+                      aria-label={
+                        showPassword ? "Hide password" : "Show password"
+                      }
+                    >
+                      {showPassword ? (
+                        <EyeOff className="h-5 w-5" />
+                      ) : (
+                        <Eye className="h-5 w-5" />
+                      )}
+                    </button>
+                  </div>
+                  {errors.password && (
+                    <p className="text-red-500">{errors.password.message}</p>
+                  )}
+                </div>
+              </div>
+              <Button
+                disabled={isSubmitting}
+                className="w-full bg-gradient-to-r from-[#335441] to-[#46704A] hover:from-[#46704A] hover:to-[#6B8F60] text-white shadow-lg transition-all duration-200 hover:shadow-xl"
+                type="submit"
+              >
+                {isSubmitting && <Loader2 className="h-5 w-5 animate-spin" />}{" "}
+                Sign Up
+              </Button>
+            </form>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t border-gray-300" />
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-white px-2 text-gray-500">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <SocialButtons />
+            </div>
+            <div className="text-center text-sm text-[#6B8F60]">
+              Already have an account?{" "}
+              <Link
+                to="/Login"
+                className="font-medium text-[#335441] hover:text-[#46704A] hover:underline"
+              >
+                Log In
+              </Link>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpForm;
